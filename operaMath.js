76fb9f7aefba1f99f1bfd81d78aa38dbc1ca7de7
Bruno <62807696+Wick096@users.noreply.github.com>
2022-01-27 16:31:01 -0300

    Add files via upload

---
+++ operaMath.js
@@ -0,0 +1,17 @@
+function soma (x,y){
+    return x+y
+}
+
+function subtracao (x,y){
+    return x-y
+}
+
+function divisao (x,y){
+    return x/y
+}
+
+function multi(x,y){
+    return x*y
+}
+
+console.log('O Resultado das operações são:','soma:'+soma(5,10),'subtracao:'+subtracao(10,8),'divisao:'+divisao(10,2),'multiplicação:'+multi(25,5))
