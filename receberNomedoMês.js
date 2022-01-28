ed3c0325930044e6d6a8b04a50d301b6b9edd63d
Bruno <62807696+Wick096@users.noreply.github.com>
2022-01-28 16:43:23 -0300

    Add files via upload

---
+++ receberNomedoMês.js
@@ -0,0 +1,5 @@
+function receberNomeDoMes(numero) {
+    const mapeamento = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho',
+    'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+    return mapeamento[--numero];
+    }
