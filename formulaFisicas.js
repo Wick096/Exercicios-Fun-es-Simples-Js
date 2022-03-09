d1759f5fd87ba04e9035d3d2bd74ed8a5f89fb6a
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-09 15:03:41 -0300

    Add files via upload

---
+++ formulaFisicas.js
@@ -0,0 +1,35 @@
+function trabalho(massa,velocidade) {
+ 
+  
+ return ((massa)*(velocidade*velocidade))/2   
+
+ 
+} 
+
+
+
+function velocidade(acelaracao,tempo){
+ 
+  
+ return (acelaracao*tempo)
+
+
+}
+
+
+
+function massa(valorMassa){
+  
+  
+return (valorMassa*1000)
+
+
+}
+
+
+
+console.log(`Valor da força trabalho é:${trabalho(80,65)}`)
+
+console.log(`Valor da Velocidade é:${velocidade(60,6)}`)
+
+console.log(`Valor da massa é:${massa(90)}`)
