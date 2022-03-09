81a0b5ef742eb273546cd99b33abc66be5a0d64f
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-09 10:55:39 -0300

    3°Commit

---
+++ diariaLocacaoCarro.js
@@ -0,0 +1,10 @@
+function locacaoCarro(txFixa,nDias){
+     
+return ((txFixa)-(txFixa*0.10)) + ((nDias*0.10)*nDias) 
+ 
+}
+ 
+ 
+
+console.log(`O valor total da locação R$:${locacaoCarro(10,30)}`)
+
