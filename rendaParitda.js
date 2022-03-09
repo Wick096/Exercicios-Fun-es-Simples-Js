917f031701c9f4df3c7b5494cc715789c038d31f
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-09 11:04:04 -0300

    Add files via upload

---
+++ rendaParitda.js
@@ -0,0 +1,14 @@
+function rendaPartida(ingPopular,ingArquibancada,ingGeral){
+  
+  
+const somadeTodosIng =((ingPopular*0.1)+(ingArquibancada*0.50)+(ingGeral*0.20))
+    
+   
+return somadeTodosIng
+
+
+}
+
+
+
+console.log(`O valor tota do jogo foi:R$ ${rendaPartida(20,50,15)}`)
