2b7abeec859b1e18cabc9c2d3916942ed4d43a7c
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-09 10:47:12 -0300

    Add files via upload

---
+++ diariaHotel.js
@@ -0,0 +1,21 @@
+ function valorEstadiaNormal(diaria){
+   
+  return (42*0,4) *8* diaria
+ 
+}
+ 
+
+function valorEstadiaPromocao(diaria){
+    
+ return (42*0,1)*8*diaria
+ 
+}
+ 
+
+ 
+
+
+console.log(`O valor da diária normal é R$: ${valorEstadiaNormal(120)}`)
+
+ 
+console.log(`O valor da diária com promoção é R$:${valorEstadiaPromocao(120)}`)
