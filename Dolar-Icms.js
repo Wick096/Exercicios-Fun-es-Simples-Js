c85e516eba7712543a163d8c54f27cd00eaa6247
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-09 10:33:29 -0300

    Add files via upload

---
+++ Dolar-Icms.js
@@ -0,0 +1,22 @@
+function converteEmDolar(valorMoeda,cotacaoDolar){
+    
+
+return valorMoeda*cotacaoDolar
+
+
+}
+
+
+
+function icms(conv,pLucro,im){
+    
+
+return ((conv*im/100)+(conv*pLucro/100))+ conv
+
+
+}
+
+
+console.log(converteEmDolar(200,5.06))
+
+console.log(icms(10,20,30))
