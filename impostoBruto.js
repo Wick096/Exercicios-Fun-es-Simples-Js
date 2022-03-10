99992ff699ddc8491d4d3e3bdcca016182963d34
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-10 15:31:51 -0300

    Add files via upload

---
+++ impostoBruto.js
@@ -0,0 +1,11 @@
+function impostoBruto(salFunc,calculoImp)
+{
+  
+  return ((salFunc*0.20) + (calculoImp*0.4)+calculoImp)
+      
+
+}
+
+
+
+console.log(`O valor a ser pago é: R$${impostoBruto(10,20)}`)
