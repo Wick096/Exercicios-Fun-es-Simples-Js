5a7b39ddd1e4c881637554417d61672486454d57
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-14 16:50:37 -0300

    Add files via upload

---
+++ aprovadoOuN.js
@@ -0,0 +1,16 @@
+function media (n1,n2,n3){
+  var n = (n1+n2+n3)/3
+
+  if(n > 7){
+    console.log(`Aprovado com Média: ${n}`)
+    
+} else {
+    console.log(`Em final com Média: ${n}`)
+}
+
+}
+
+console.log(media(7,8,9))
+
+
+
