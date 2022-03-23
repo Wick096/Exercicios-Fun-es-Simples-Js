a8fa31fee736b8b07fc474b536947dbeaa9c4265
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-23 15:43:36 -0300

    Add files via upload

---
+++ lucro.js
@@ -0,0 +1,25 @@
+const lucro = (precoCompra,precoVenda) => precoCompra-precoVenda
+ 
+ 
+ 
+
+let mercadoria = {
+     
+nomeProduto:'Celular',
+     
+marca:'Apple',
+     
+precoCompra:'R$1520',
+     
+precoVenda: 'R$900'
+
+
+}
+ 
+ 
+
+
+
+
+
+console.log(`A mercadoria ${mercadoria.nomeProduto} teve lucro de R$:${lucro(1520,900)}` )
