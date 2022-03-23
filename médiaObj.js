d05d7a44a9b985991936f186f6c7e77dadae81e5
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-23 15:25:14 -0300

    Add files via upload

---
+++ médiaObj.js
@@ -0,0 +1,28 @@
+function media(n1,n2,n3){
+   
+ return (n1+n2+n3)/3
+ 
+}
+
+   
+
+let aluno = {
+    
+
+ nome:'Paulo',
+    
+ nota1:8,
+    
+ nota2:5,
+    
+ nota3:1
+    
+
+
+};
+
+
+
+
+
+console.log(`O aluno ${alunos.nome} teve média ${media(8,5,1)}` )
