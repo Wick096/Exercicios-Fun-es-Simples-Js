4dbb475bf5d54c443e53f9a9ce17391f2de6cd3a
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-30 14:21:42 -0300

    Add files via upload

---
+++ verificaIdade.js
@@ -0,0 +1,24 @@
+function verificaIdade(idade) 
+{
+    
+if(idade < 16)
+{
+        
+console.log("Não pode tirar o título de eleitor.")
+   
+ }
+    
+else if(idade >=16){
+      
+  console.log("Pode tirar o título de eleitor.")
+    
+}
+
+
+
+}
+
+
+
+
+console.log(verificaIdade(18))
