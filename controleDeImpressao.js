8bcea60b36427c88b2fc89f32024f2c6a436f910
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-30 14:16:58 -0300

    Add files via upload

---
+++ controleDeImpressao.js
@@ -0,0 +1,38 @@
+
+function impressora (qtdPages){
+    
+	if(qtdPages == 2){
+    
+	return (qtdPages *0.20)
+   
+ }
+    
+	else if(qtdPages == 4){
+
+        
+	
+	return (qtdPages *0.40)
+
+    
+}
+    
+    
+     else if(qtdPages > 4){
+       
+        
+	return (qtdPages *1.50)
+    
+	
+	}
+
+    
+
+
+}
+
+
+
+
+
+
+console.log(impressora(5))
