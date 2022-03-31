f5a50fb40a282ca53df607a645125c6308a67d77
Bruno <62807696+Wick096@users.noreply.github.com>
2022-03-31 14:43:32 -0300

    Add files via upload

---
+++ concentracaoMolar.js
@@ -0,0 +1,11 @@
+function concentracaoMolar(massa,massaMolar,velocidade){
+    
+
+return (massa/massaMolar)*velocidade
+
+
+}
+
+
+
+console.log(`A concentração molar é: ${concentracaoMolar(8,10,5)}`)
